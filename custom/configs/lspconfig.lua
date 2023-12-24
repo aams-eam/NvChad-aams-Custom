5128485e3a257d114a91488bd0297ab08022acc4
Alejandro A. Moreno Sancho <aams.eam@gmail.com>
2023-12-24 17:48:24 +0100

    Add base configuration

---
+++ custom/configs/lspconfig.lua
@@ -0,0 +1,17 @@
+local config = require("plugins.configs.lspconfig")
+
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require("lspconfig")
+
+lspconfig.pylsp.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
+
+lspconfig.ltex.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
